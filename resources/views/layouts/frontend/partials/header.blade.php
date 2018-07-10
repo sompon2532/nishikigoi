fad5ed1e8f56e454f9dbeeca8535201bf2d2c56f
mmmai <nit.sukhom@gmail.com>
2018-07-10 15:54:56 +0700

    Banner

--- resources/views/layouts/frontend/partials/header.blade.php
+++ resources/views/layouts/frontend/partials/header.blade.php
@@ -2,17 +2,22 @@
 <div class="logo-box">
     <div class="col-md-12">
         <a href="{{ route('frontend.index') }}" class="logo">
-            <img class="img-responsive" src="{{ asset('frontend/img/NISHIKIGOI_LOGO.png') }}" alt="">
+            <img class="img-responsive" src="{{ asset('frontend/img/AW-NISHIKIGOI_ALLIANCE-BANNER.jpg') }}" alt="">
         </a>
-    </div>
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <a href="{{ route('frontend.event.index') }}" class="btn-event">
-            <img class="img-responsive" src="{{ Request::segment(1) == 'event' ? asset('frontend/Icon/Event-Red.png') :asset('frontend/Icon/Event-Gray.png') }}" alt="" width="60px">
-        </a>
-    </div>
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <a href="{{ route('frontend.partner.index') }}" class="btn-partner">
-            <img class="img-responsive" src="{{ Request::segment(1) == 'partner' ? asset('frontend/Icon/Alliance-Red.png') : asset('frontend/Icon/Alliance-Gray.png') }}" alt="" width="60px">
-        </a>
+        <div class="row">
+
+            <div class="logo-box-btn">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <a href="{{ route('frontend.event.index') }}" class="btn-event">
+                        <img class="img-responsive" src="{{ Request::segment(1) == 'event' ? asset('frontend/Icon/Event-Red.png') :asset('frontend/Icon/Event-Gray.png') }}" alt="" width="60px">
+                    </a>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <a href="{{ route('frontend.partner.index') }}" class="btn-partner">
+                        <img class="img-responsive" src="{{ Request::segment(1) == 'partner' ? asset('frontend/Icon/Alliance-Red.png') : asset('frontend/Icon/Alliance-Gray.png') }}" alt="" width="60px">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
